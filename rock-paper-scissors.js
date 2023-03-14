3e7b81caccdb8941d57fc1f96b6ff31821eb4625
terNoKi <polik0ternoki@gmail.com>
2023-03-15 00:36:32 +0100

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,38 @@
-const choices = ["Rock", "Paper", "Scissors"];
+let choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(){
-    let index = Math.floor(Math.random(choices.length));
+    let index = Math.floor(Math.random()*choices.length);
     return console.log(`The computer chooses: ${choices[index]}!`);
 }
-getComputerChoice();
+let computerChoice = getComputerChoice();
+function getPlayerChoice(){
+    let sign = window.prompt("What do you choose?", "Rock, Paper or Scissors?")
+    let choice = ""
+    if (sign.toLowerCase() === "rock"){
+        alert("Okay! Rock it is!")
+        choice = "Rock"
+    }else if (sign.toLowerCase() === "paper"){
+        alert("Okay! Paper it is!")
+        choice = "Paper"
+    }else if (sign.toLowerCase() === "scissors"){
+        alert("Okay! Scissors it is!")
+        choice = "Scissors"
+    }else{
+        alert("Hey, pick a correct option!")
+    }
+    return console.log(`You chose: ${choice}!`)
+}
+let playerChoice = getPlayerChoice();
+console.log(playerChoice)
+// make a power list (elif statement)
+// get user choice function
+// make rounds for player and comuter choice
+// create a round counter
+// for loop probably
+// set up a winner/looser screen
+
+
+
+
+for (round = 1; round > 5; round++){
+
+}
