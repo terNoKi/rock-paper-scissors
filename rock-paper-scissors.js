aa812b1a773a61154b6d9ef4ae9c5fbecb411346
terNoKi <polik0ternoki@gmail.com>
2023-03-15 23:49:23 +0100

    added a css file

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -27,13 +27,8 @@ function game(){
 function getComputerChoice(){
     let index = Math.floor(Math.random()*choices.length);
     alert(`The computer made a choice!`);
-<<<<<<< Updated upstream
-    console.log(choices[index])
-    return choices[index]
-=======
     console.log(`Computer choice: ${choices[index]}`);
     return choices[index];
->>>>>>> Stashed changes
 }
 function getPlayerChoice(){
     let sign = window.prompt("What do you choose?", "Rock, Paper or Scissors?")
@@ -50,47 +45,14 @@ function getPlayerChoice(){
     }else{
         alert("Hey, pick a correct option!")
     }
-<<<<<<< Updated upstream
-    alert(`You chose: ${choice}!`)
-    console.log(choice)
-    return choice
-=======
     alert(`You chose: ${choice}!`);
     console.log(`Player choice: ${choice}`);
     return choice;
->>>>>>> Stashed changes
 }
 
 function playRound(playerChoice, computerChoice){
         let sign = alert("The fight begins!")
         if (playerChoice === computerChoice){
-<<<<<<< Updated upstream
-            alert("It's a draw!")
-        }else if (playerChoice === "Rock" && computerChoice === "Paper"){
-            alert("Computer wins!")
-        }else if (playerChoice === "Paper" && computerChoice === "Scissors"){
-            alert("Computer wins!")
-        }else if (playerChoice === "Scissors" && computerChoice === "Rock"){
-            alert("Computer wins!")
-        }else{
-            alert("Player wins!")
-        }
-
-
-
-}
-let round = playRound(playerChoice, computerChoice);
-
-
-
-
-// make a power list (elif statement)
-// get user choice function
-// make rounds for player and comuter choice
-// create a round counter
-// for loop probably
-// set up a winner/looser screen
-=======
             alert(`It's a draw! Computer also chose ${computerChoice}!`);
             state = "draw"
         }else if (playerChoice === "Rock" && computerChoice === "Paper"){
@@ -113,9 +75,8 @@ let round = playRound(playerChoice, computerChoice);
 
 let output = game()
 alert(`The winner is: ${output}`)
-console.log(`The winner is computer ${output}`)
+console.log(`The winner is ${output}`)
 
->>>>>>> Stashed changes
 
 
 
